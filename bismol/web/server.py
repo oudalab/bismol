6b74f75a7227f6f9462482a6063aaa6a71378fb5
Josh Imbriani <joshimbriani@gmail.com>
2016-02-15 23:57:19 -0600

    Added start of D3 graph and finshed serving data via Flask

--- bismol/web/server.py
+++ bismol/web/server.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route("/")
-def hello():
-	return "Hello World"
+def hello1():
+	return render_template('home.html')
 
-def startWebServer():
-	app.run()
+@socketio.on('message')
+def handle_message(message):
+	print('received message: ' + message)
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+	print('received json: ' + str(json))
+	return 'one', 2
+
+@socketio.on('connect')
+def test_connect():
+	print('my response', {'data': 'Connected', 'count': 0})
+
+socketio.run(app)
