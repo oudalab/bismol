dcfe71ec53ec4ae4bd96b56be5e7cb8baaaa5df8
Christan Grant <cgrant@ou.edu>
2016-10-17 14:13:06 -0500

    Working gettweet code! ..need to test in kubernetes again.

--- ingestor/gettweets.py
+++ ingestor/gettweets.py
@@ -1,69 +1,65 @@
 #!/usr/bin/python3
+"""This script takes uses the twitter stream and keywords to
+pull in tweets."""
+
 import argparse
 import io
 import json
 from json import dumps
-import logging
-import os
 from os import environ
-import psycopg2
-from psycopg2 import extras 
 import sys
+import time
 import tweepy
 
 
-RUNID = environ['RUNID'] or "DEFAULT"
+RUNID = environ.get('RUNID') or "DEFAULT"
+FILENAME = environ.get('TWEET_FILE') or \
+        'tweet_{}.json'.format(time.strftime("%Y%m%d%H%M%S"))
 
-log = logging.getLogger(RUNID)
-log.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s %(levelname)s:%(name)s %(message)s")
-ch.setFormatter(formatter)
-log.addHandler(ch)
 
-# Twitter API info: https://dev.twitter.com/streaming/overview/request-parameters#track
+# Twitter API info:
+#  https://dev.twitter.com/streaming/overview/request-parameters#track
 # Tweepy github: https://github.com/tweepy/tweepy/tree/master/tweepy
 
-# Note: All environment variables should be 
+# Note: All environment variables should be
 # docker run --env-file twitter.env --env-file database.env
 # Usage: python3 gettweets.py | tee -a tweets.json.gz
 
 # Connect to twitter.com/oudalab bismol app
 
-INSERT = """INSERT INTO tweets (tweetid, runid, status, statusvec, tweet)
-            VALUES (%s, %s, %s, to_tsvector('english', %s), %s)"""
 
 class HealthStreamListener(tweepy.StreamListener):
+    """Extended Steam listener for these health tweets."""
 
-    def __init__(self, api=None):
-        # Connect to the DB
-        self.conn = psycopg2.connect("host ='{PGHOST}' dbname='{PGDATABASE}' "
-            "user='{PGUSER}' password='{PGPASSWORD}' port='{PGPORT}'"
-            .format(**environ))
-        super(HealthStreamListener, self).__init__(api) # Python 3
+    # def __init__(self,api=None):
+    def __init__(self, api):
+        super(HealthStreamListener, self).__init__(api)  # Python 3
+        self.twfile = io.open(FILENAME, 'w', encoding="utf-8")
+        print('__init__ {}'.format(FILENAME), file=sys.stderr)
 
     def on_status(self, status):
-        # Insert the data into postgresql
-        cur = self.conn.cursor()
-        cur.execute(INSERT, (status.id, RUNID, status.text, status.text, json.dumps(status._json)))
-        self.conn.commit()
-        cur.close()
-        print('{}'.format(dumps(status._json)), file=sys.stderr)
+        # print('{}'.format(dumps(status._json)), file=sys.stderr)
+        print('{}'.format(dumps(status._json)), file=self.twfile)
+        sys.stderr.write('.')
 
     def on_error(self, status_code):
-        # Close database connection
-        self.conn.close()
         if status_code == 420:
-            #returning False in on_data disconnects the stream
+            # returning False in on_data disconnects the stream
+            print('Caught an error :-({}'.format(status_code), file=sys.stderr)
             return False
+        else:
+            print('Caught an error :-({}'.format(status_code), file=sys.stderr)
+            return True
+
 
 # Get the twitter key words
-def get_keywords(kw_file = 'key-words.json'):
-    with io.open(kw_file, 'r') as f:
-        kw = json.load(f)
-        words = ["{} {}".format(a,b) for a in kw['column1'] for b in kw['column2']]
-        words += kw['diseases']
+def get_keywords(kw_file='key-words.json'):
+    """Makes an an exhaustive pair of keywords from both lists."""
+    with io.open(kw_file, 'r') as my_file:
+        kws = json.load(my_file)
+        words = ["{} {}".format(a.strip(), b.strip())
+                 for a in kws['column1'] for b in kws['column2']]
+        words += kws['diseases']
         return words
 
 
@@ -71,21 +67,21 @@ if __name__ == "__main__":
     # Run as a script
     parser = argparse.ArgumentParser()
     parser.add_argument("-ck", "--consumerkey", dest="ck",
-                        default=environ['TWITTER_CONSUMER_KEY'],
+                        default=environ.get('TWITTER_CONSUMER_KEY'),
                         help="The twitter consumer key")
     parser.add_argument("-cs", "--consumersecret", dest="cs",
-                        default=environ['TWITTER_CONSUMER_SECRET'],
+                        default=environ.get('TWITTER_CONSUMER_SECRET'),
                         help="The twitter consumer secret")
     parser.add_argument("-at", "--accesstoken", dest="at",
-                        default=environ['TWITTER_ACCESS_TOKEN'],
+                        default=environ.get('TWITTER_ACCESS_TOKEN'),
                         help="The twitter access token")
     parser.add_argument("-ats", "--accesstokensecret", dest="ats",
-                        default=environ['TWITTER_ACCESS_TOKEN_SECRET'],
+                        default=environ.get('TWITTER_ACCESS_TOKEN_SECRET'),
                         help="The twitter access token secret")
 
     # Option to be flexibe with time outs and ssl errors
     # https://github.com/ryanmcgrath/twython/issues/273
-    client_args = { 'verify': False }
+    client_args = {'verify': False}
 
     args = parser.parse_args()
 
@@ -95,12 +91,13 @@ if __name__ == "__main__":
     api = tweepy.API(auth, compression=True, wait_on_rate_limit=True)
 
     myStreamListener = HealthStreamListener(api)
-    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+    print("Keywords:\n{}".format(get_keywords()), file=sys.stderr)
 
     # Run the stream continually, try again on failure
     while True:
         try:
             myStream.filter(track=get_keywords(), async=True)
-        except:
+        except Exception as e:
             continue
-
