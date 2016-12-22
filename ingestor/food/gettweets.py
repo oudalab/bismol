89985b06995a9cb9e54a452db3c7bcc31d1ea2e7
Christan Grant <cgrant@ou.edu>
2016-12-22 15:11:08 -0600

    Added paging for food tweet processing

--- ingestor/food/gettweets.py
+++ ingestor/food/gettweets.py
@@ -63,53 +63,68 @@ def get_keywords(kw_file='foodlist.txt'):
         return [t.strip() for t in myf.readlines()]
 
 
-if __name__ == "__main__":
-    # Run as a script
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-ck", "--consumerkey", dest="ck",
-                        default=environ.get('TWITTER_CONSUMER_KEY'),
-                        help="The twitter consumer key")
-    parser.add_argument("-cs", "--consumersecret", dest="cs",
-                        default=environ.get('TWITTER_CONSUMER_SECRET'),
-                        help="The twitter consumer secret")
-    parser.add_argument("-at", "--accesstoken", dest="at",
-                        default=environ.get('TWITTER_ACCESS_TOKEN'),
-                        help="The twitter access token")
-    parser.add_argument("-ats", "--accesstokensecret", dest="ats",
-                        default=environ.get('TWITTER_ACCESS_TOKEN_SECRET'),
-                        help="The twitter access token secret")
-
-    # Option to be flexibe with time outs and ssl errors
-    # https://github.com/ryanmcgrath/twython/issues/273
-    client_args = {'verify': False}
-
-    args = parser.parse_args()
+# Run the program
+def start(args):
+    """Run the whole food scraper program block by block"""
 
     if None in (args.ck, args.at, args.cs, args.ats):
         print("Error defining environment variables", file=sys.stderr)
         sys.exit()
+
+    args.ind = int(args.ind)
+    args.b = int(args.b)
 
     auth = tweepy.OAuthHandler(args.ck.strip(), args.cs.strip())
     auth.set_access_token(args.at.strip(), args.ats.strip())
 
     api = tweepy.API(auth, compression=True, wait_on_rate_limit=True)
 
-    myStreamListener = FoodStreamListener(api)
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+    mystreamlistener = FoodStreamListener(api)
+    mystream = tweepy.Stream(auth=api.auth, listener=mystreamlistener)
 
     print("Keywords:\n{}".format(get_keywords()), file=sys.stderr)
 
     # English only tweets
     languages = ["en"]
     # US Bounding box
-    locations = [-125.4113785028, 24.686952412,
-                 -65.91796875, 49.382372787]
+    # locations = [-125.4113785028, 24.686952412,
+    #             -65.91796875, 49.382372787]
     # Run the stream continually, try again on failure
     while True:
         try:
-            myStream.filter(track=get_keywords(),
+            if args.b != -1:
+                track = get_keywords()[args.ind*args.b: args.b*args.ind+args.b]
+            else:
+                track = get_keywords()
+
+            mystream.filter(track=track,
                             languages=languages,
-                            locations=locations,
+                            # locations=locations,
                             async=True)
-        except Exception as e:
+        except:
             continue
+
+
+if __name__ == "__main__":
+    # Run as a script
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("-ck", "--consumerkey", dest="ck",
+                        default=environ.get('TWITTER_CONSUMER_KEY'),
+                        help="The twitter consumer key")
+    PARSER.add_argument("-cs", "--consumersecret", dest="cs",
+                        default=environ.get('TWITTER_CONSUMER_SECRET'),
+                        help="The twitter consumer secret")
+    PARSER.add_argument("-at", "--accesstoken", dest="at",
+                        default=environ.get('TWITTER_ACCESS_TOKEN'),
+                        help="The twitter access token")
+    PARSER.add_argument("-ats", "--accesstokensecret", dest="ats",
+                        default=environ.get('TWITTER_ACCESS_TOKEN_SECRET'),
+                        help="The twitter access token secret")
+    PARSER.add_argument("-ind", "--index", dest="index", type=int,
+                        default=environ.get('KEYWORD_INDEX') or 1,
+                        help="The index to the keyword set to be streamed")
+    PARSER.add_argument("-b", "--block", dest="block_size", type=int,
+                        default=environ.get('KEYWORD_BLOCK_SIZE') or -1,
+                        help="The index to the keyword set to be streamed")
+
+    start(PARSER.parse_args())
