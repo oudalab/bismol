f737919d6cda9b9ab81a19e9240a82d3f1da1983
Christan Grant <cgrant@ou.edu>
2018-10-26 09:58:48 -0500

    Added new africa ingestor for @ensoesie. Need to update the bounding box. Also, needs to switch to compression

---
+++ ingestor/africa/gettweets.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+"""This script takes uses the twitter stream and keywords to
+pull in tweets."""
+
+import argparse
+import io
+from json import dumps
+from os import environ
+import sys
+import time
+import tweepy
+
+
+RUNID = environ.get('RUNID') or "USADEFAULT"
+FILENAME = environ.get('TWEET_FILE') or \
+        '/data/tweetsdb/tweet_africa_{}.json'.format(time.strftime("%Y%m%d%H%M%S"))
+
+
+# Twitter API info:
+#  https://dev.twitter.com/streaming/overview/request-parameters#track
+# Tweepy github: https://github.com/tweepy/tweepy/tree/master/tweepy
+
+# Note: All environment variables should be
+# docker run --env-file twitter.env --env-file database.env
+# Usage: python3 gettweets.py | tee -a tweets.json.gz
+
+# Connect to twitter.com/oudalab bismol app
+
+
+class FoodStreamListener(tweepy.StreamListener):
+    """Extended Steam listener for these food tweets."""
+
+    # def __init__(self,api=None):
+    def __init__(self, api):
+        super(FoodStreamListener, self).__init__(api)  # Python 3
+        self.twfile = io.open(FILENAME, 'w', encoding="utf-8")
+        print('__init__ {}'.format(FILENAME), file=sys.stderr)
+
+    def on_status(self, status):
+        """This is depreciated and not actually used anymore"""
+        # print('{}'.format(dumps(status._json)), file=sys.stderr)
+        print('{}'.format(dumps(status._json)), file=self.twfile)
+        sys.stderr.write('.')
+
+    def on_data(self, raw_data):
+        print('{}'.format(raw_data), file=self.twfile)
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            print('Caught an error :-({}'.format(status_code), file=sys.stderr)
+            return False
+        else:
+            print('Caught an error :-({}'.format(status_code), file=sys.stderr)
+            return True
+
+
+# Run the program
+def start(args):
+    """Run the whole food scraper program block by block"""
+
+    if None in (args.ck, args.at, args.cs, args.ats):
+        print("Error defining environment variables, {}".format(args), file=sys.stderr)
+        sys.exit()
+
+    
+    args.ind = int(args.ind)
+    args.b = int(args.b)
+
+    auth = tweepy.OAuthHandler(args.ck.strip(), args.cs.strip())
+    auth.set_access_token(args.at.strip(), args.ats.strip())
+
+    api = tweepy.API(auth, compression=True, wait_on_rate_limit=True)
+
+    mystreamlistener = FoodStreamListener(api)
+    mystream = tweepy.Stream(auth=api.auth, listener=mystreamlistener)
+
+    print("Keywords:\n{}".format(get_keywords()), file=sys.stderr)
+
+    # English only tweets
+    languages = ["en"]
+    # US Bounding box
+    locations = [-125.4113785028, 24.686952412, -65.91796875, 49.382372787]
+    # Run the stream continually, try again on failure
+    while True:
+        try:
+            mystream.filter(languages=languages, locations=locations, async=True)
+        except:
+            continue
+
+
+if __name__ == "__main__":
+    # Run as a script
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("-ck", "--consumerkey", dest="ck",
+                        default=environ.get('TWITTER_CONSUMER_KEY'),
+                        help="The twitter consumer key")
+    PARSER.add_argument("-cs", "--consumersecret", dest="cs",
+                        default=environ.get('TWITTER_CONSUMER_SECRET'),
+                        help="The twitter consumer secret")
+    PARSER.add_argument("-at", "--accesstoken", dest="at",
+                        default=environ.get('TWITTER_ACCESS_TOKEN'),
+                        help="The twitter access token")
+    PARSER.add_argument("-ats", "--accesstokensecret", dest="ats",
+                        default=environ.get('TWITTER_ACCESS_TOKEN_SECRET'),
+                        help="The twitter access token secret")
+    PARSER.add_argument("-ind", "--index", dest="ind", type=int,
+                        default=environ.get('TWITTER_INDEX') or 1,
+                        help="The index to the keyword set to be streamed")
+
+    start(PARSER.parse_args())
